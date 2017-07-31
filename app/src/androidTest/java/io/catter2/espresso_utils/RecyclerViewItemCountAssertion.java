69e809d8739dedda27c1c2eb0388997b39f2fcce
Allan Hasegawa <hasegawa.aran@gmail.com>
2017-07-31 17:50:42 -0300

    First attempt at testing FavoritesActivity

---
+++ app/src/androidTest/java/io/catter2/espresso_utils/RecyclerViewItemCountAssertion.java
@@ -0,0 +1,28 @@
+package io.catter2.espresso_utils;
+
+import android.support.test.espresso.NoMatchingViewException;
+import android.support.test.espresso.ViewAssertion;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class RecyclerViewItemCountAssertion implements ViewAssertion {
+    private final int expectedCount;
+
+    public RecyclerViewItemCountAssertion(int expectedCount) {
+        this.expectedCount = expectedCount;
+    }
+
+    @Override
+    public void check(View view, NoMatchingViewException noViewFoundException) {
+        if (noViewFoundException != null) {
+            throw noViewFoundException;
+        }
+
+        RecyclerView recyclerView = (RecyclerView) view;
+        RecyclerView.Adapter adapter = recyclerView.getAdapter();
+        assertThat(adapter.getItemCount(), is(expectedCount));
+    }
+}
